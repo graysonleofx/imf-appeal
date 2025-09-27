8e82e996bc4521fd60a75e663b478baccd73e99f
Leo Grayson <leograyson1969@gmail.com>
2025-09-27 12:07:30 +0100

    Next-auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -29,32 +29,4 @@ const handler = NextAuth({
 })
 
 
-// export const authOptions = {
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//         authorization: {
-//           params: {
-//             scope: "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/mail.google.com",
-//           },
-//         },
-//     }),
-//   ],
-//   callbacks: {
-//     async jwt({ token, account }) {
-//       if (account) {
-//         token.accessToken = account.access_token;
-//       }
-//       return token;
-//     },
-//     async session({ session, token }) {
-//       session.accessToken = token.accessToken;
-//       return session;
-//     }
-//   },
-//   secret: process.env.NEXTAUTH_SECRET,
-// };
-// export default NextAuth(authOptions);
-
 export {handler as GET, handler as POST}
