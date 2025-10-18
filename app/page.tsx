c951b27d21d299a3800659869b636ebf7412f909
Leo Grayson <leograyson1969@gmail.com>
2025-10-18 23:25:14 +0100

    updated the UI to current

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Header } from "@/components/header"
 import  MainSection  from "@/components/MainSection.jsx"
 import HeroSection from "@/components/hero-section"
-import { AppealSection } from "@/components/appeal-section"
+import { AppealSection } from "@/components/appeal-section.jsx"
 import { Footer } from "@/components/footer"
 
 export default function HomePage() {
